e5479966654fa8d239fabc5605ec12858c4df570
Adam Czapla <adam@adamczapla.de>
2022-01-07 20:28:17 +0100

    class Watermelon added.

---
+++ Header Files/800/Watermelon.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class Watermelon {
+public:
+    inline void solve()
+    {
+        std::cout << "Watermelon solved." << std::endl;
+    }
+};
---
+++ Source Files/main.cpp
@@ -0,0 +1,9 @@
+#include "800/Watermelon.h"
+
+int main(int argc, char* argv[])
+{
+    Watermelon m;
+    m.solve();
+
+    return 0;
+}
