cbea5f995d0481444371181738cd96a8eaa0e709
Adam Czapla <adam@adamczapla.de>
2022-01-08 11:49:54 +0100

    no comment

--- Header Files/800/Watermelon.h
+++ Header Files/800/Watermelon.h
@@ -1,9 +1,24 @@
-#include <iostream>
+// #include <iostream>
 
-class Watermelon {
-public:
-    inline void solve()
-    {
-        std::cout << "Watermelon solved." << std::endl;
-    }
-};
+// using namespace std;
+
+// class Watermelon {
+// public:
+//     inline void solve()
+//     {
+//         int w;
+//         cin >> w;
+//         if (w % 2 || w == 2) {
+//             cout << "NO" << endl;
+//         } else {
+//             cout << "YES" << endl;
+//         }
+//     }
+// };
+
+// int main(int argc, char* argv[])
+// {
+//     Watermelon w;
+//     w.solve();
+//     return 0;
+// }_
--- Source Files/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "800/Watermelon.h"
-
-int main(int argc, char* argv[])
-{
-    Watermelon m;
-    m.solve();
-
-    return 0;
-}
