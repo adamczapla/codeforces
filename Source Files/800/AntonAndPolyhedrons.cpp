f9170c2983a8094b37ca303fca14388cc4e053a9
Adam Czapla <adam@adamczapla.de>
2022-01-13 17:51:01 +0100

    added AntonAndPolyhedrons.cpp

---
+++ Source Files/800/AntonAndPolyhedrons.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class AntonAndPolyHedrons {
+
+public:
+
+    inline void solve()
+    {
+        unordered_map<string, unsigned> polyh_set = {
+            {"Tetrahedron", 4},
+            {"Cube", 6},
+            {"Octahedron", 8},
+            {"Dodecahedron", 12},
+            {"Icosahedron", 20}
+        };
+
+        unsigned n, cnt = 0;
+        cin >> n;
+
+        while (n--) {
+            string polyh;
+            cin >> polyh;
+            auto pos = polyh_set.find(polyh);
+            if (pos != polyh_set.cend()) {
+                cnt += pos->second;
+            }
+        }
+
+        cout << cnt << endl;
+    }
+
+};
+
+int main(int argc, char* argv[])
+{
+    AntonAndPolyHedrons p;
+    p.solve();
+    return 0;
+}
