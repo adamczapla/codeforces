cabcbf8f273cf4b2b9636e5e3d2866cc5708486e
Adam Czapla <adam@adamczapla.de>
2022-01-13 10:39:02 +0100

    better version of HitTheLottery.cpp

--- Source Files/800/HitTheLottery.cpp
+++ Source Files/800/HitTheLottery.cpp
@@ -10,17 +10,15 @@ public:
     inline void solve()
     {
         const vector<unsigned> bills = {100, 20, 10, 5, 1};
-        unsigned long long n, counter = 0, pos = 0;
+        unsigned long long n, cnt = 0, pos = 0;
+
         cin >> n;
 
-        while (n != 0) {
-            unsigned long long quotient =  n / bills[pos];
-            n = n - (quotient * bills[pos]);
-            counter += quotient;
-            ++pos;
-        }
+        do {
+            cnt += n / bills[pos];
+        } while ((n %= bills[pos++]) != 0);
 
-        cout << counter << endl;
+        cout << cnt << endl;
     }
 
 };
