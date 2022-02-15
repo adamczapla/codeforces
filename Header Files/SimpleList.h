da8fc76284199076177f0207aa3aa2345a88451c
Adam Czapla <adam@adamczapla.de>
2022-02-15 18:30:47 +0100

    changed SimpleList.cpp and SimpleList.h

--- Header Files/SimpleList.h
+++ Header Files/SimpleList.h
@@ -1,13 +1,38 @@
 #ifndef SIMPLELIST_H
 #define SIMPLELIST_H
 
-#include <iostream>
+template <typename T>
+class Node;
 
 template <typename T>
 class SimpleList {
 public:
+
+    SimpleList() = default;
+
+    void push_back(const T&);
+    void pop_front();
     void print();
+
+private:
+
+    Node<T>* head = nullptr;
+    Node<T>* tail = nullptr;
 };
 
+template <typename T>
+class Node {
+
+    friend class SimpleList<T>;
+
+public:
+
+    Node(const T& v) : val(v) {}
+
+private:
+
+    Node<T>* next = nullptr;
+    T val;
+};
 
 #endif  // SIMPLELIST_H
